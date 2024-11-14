76287cc75a12beadf74ae27a825737e3f3915bd4
stinkypuff96 <darintsv@gmail.com>
2024-11-14 20:54:21 +0100

    alt buffer

--- ansi/rainbow.py
+++ ansi/rainbow.py
@@ -1,3 +1,4 @@
+import os
 import time
 from no_echo import disable_echo
 
@@ -12,15 +13,15 @@ colors = [
 ]
 color_count = len(colors)
 
-line_count = 10
 segment_width = 15
 character = '\u2588'
 frame_count = segment_width * color_count
 
-def print_rainbow(frame: int) -> None:
-    output = ''
 
-    for line in range(0, line_count):
+def print_rainbow(frame: int) -> None:
+    output = '\033[999A\r'
+    t_width, t_height = os.get_terminal_size()
+    for line in range(0, t_height):
         line_frame = (frame + line) % frame_count
         start_color = line_frame // segment_width
         offset = line_frame % segment_width
@@ -31,17 +32,19 @@ def print_rainbow(frame: int) -> None:
         output += f'\033[{colors[(start_color + color_count - 1) % color_count]}m{character * offset}'
         output += '\n'
 
-    output += f'\033[{line_count + 1}A'
-    print(output)
+    output = output[0:-1]
+
+    print(output, end='', flush=True)
 
 
 current_frame = 0
 try:
     disable_echo(enabled=False)
+    print('\033[?25l\033[?1049h\033[2J', end='', flush=True)
     while True:
         print_rainbow(current_frame)
         current_frame = (current_frame + 1) % frame_count
         time.sleep(0.1)
 except KeyboardInterrupt:
     disable_echo(enabled=True)
-    print('\033[2K\033[B'* line_count + f'\033[{line_count}A\r', end='', flush=True)
+    print(f'\033[?25h\033[?1049l', end='', flush=True)
