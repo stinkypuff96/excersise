eecc91881ae02737aa6979123c0cbb6a01b73f28
stinkypuff96 <darintsv@gmail.com>
2024-12-24 19:44:40 +0100

    Using bs4 and requests to pull article headlines

--- practice/practice9.py
+++ practice/practice9.py
@@ -8,5 +8,7 @@ r = requests.get(url)
 r_html = r.text
 
 soup = BeautifulSoup(r_html, 'html.parser')
+print("This is a list of article headlines:")
 for link in soup.find_all('div', class_='css-xdandi'):
     print(link.text)
+print("That's the end of the article headlines!")
